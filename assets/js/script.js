d97aac2eb2a8b80360ce0eb1f0bc73327e4d11ed
paulemfag <boiteprofagot@gmail.com>
2019-11-22 13:58:36 +0100

    *

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var c = document.getElementById( "c1" );
+var ctx = c.getContext("2d");
+
+// Fond
+ctx.fillStyle = "#a6d7e7";
+ctx.fillRect(100,50,100,200);
+
+ctx.fillStyle = "#c9c9c9";
+ctx.fillRect(115,70,15,15);
+ctx.fillRect(170,70,15,15);
+ctx.fillRect(140,110,20,40);
+
+// 300x150
